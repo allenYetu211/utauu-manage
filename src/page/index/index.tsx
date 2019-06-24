21515e81843ba80a588dcb9ab96f3b141e1dc177
luojinghui <luojinghui@xylink.com>
2019-06-24 20:33:51 +0800

    refactor: delete comment code and delete no use code

--- src/page/index/index.tsx
+++ src/page/index/index.tsx
@@ -3,9 +3,6 @@ import logo from 'global/asset/images/xylink-logo.png';
 import style from './style/index.scss';
 
 const IndexView = () => {
-	let a = 1;
-	console.log(123, a);
-
 	return (
 		<div className={style.container}>
 			<div className={style.containerLogo}>
